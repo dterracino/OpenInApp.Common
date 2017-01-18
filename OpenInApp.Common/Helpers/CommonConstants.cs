110ae5a814f80c0829f37e9dd1cb2e9fe21dc00a
Gmaniacuk1 <gtrevellick@googlemail.com>
2017-01-18 18:15:49 +0000

    text for asterisk for all file types

--- OpenInApp.Common/Helpers/CommonConstants.cs
+++ OpenInApp.Common/Helpers/CommonConstants.cs
@@ -35,8 +35,8 @@ namespace OpenInApp.Common.Helpers
         public const string SuppressTypicalFileExtensionsWarningDetailedDescription = "By default you will see a warning when trying to open a file with an extension that is not typically associated with the application. Setting this option to true will prevent this warning from being displayed.";
         public const string SuppressTypicalFileExtensionsWarningOptionLabel = "Suppress warning for non-typical file extensions";
         public const string ToolsOptionsNotice = "(You can change suppress this notice in Tools | Options)";
-        public const string TypicalFileExtensionsOptionDetailedDescription = "A comma-separated list of file extensions that can typically be opened in the application. You can open files with extensions that are not in this list, but will be warned first.";
-        public const string TypicalFileExtensionsOptionLabel = "Typical file extensions";
+        public const string TypicalFileExtensionsOptionDetailedDescription = "A comma-separated list of file extensions that can typically be opened in the application. You can open files with extensions that are not in this list, but will be warned first. If you enter '*' (without apostrophes) all files types will be allowed without a warning. If you leave blank a set of default file types will be re-loaded into your options.";
+        public const string TypicalFileExtensionsOptionLabel = "Typical file extensions (enter '*' for all)";
         public static string UnexpectedError =
             "An unexpected error has occured. Please restart Visual Studio and re-try." + Environment.NewLine + Environment.NewLine +
             "If the error persists please log a bug for this extension via the Visual Studio Marketplace at https://marketplace.visualstudio.com" + Environment.NewLine + Environment.NewLine +
